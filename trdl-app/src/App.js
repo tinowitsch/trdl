be55495c3c49b459f4e8b72ecc7a31e6385d1b33
tinowitsch <martinekowitsch@chello.at>
2024-05-01 23:18:34 +0200

    Bug fixes 0.1

--- trdl-app/src/App.js
+++ trdl-app/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-          
+        <p>DeepApp</p>
+        <li>button</li>
+        <li>input field</li>:console.warn();
+      </header>          
     </div>
   );
 }
