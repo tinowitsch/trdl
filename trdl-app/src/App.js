392a0ad7a91e8cfd2eb318a1a5da1dfaff8328c0
M E N <martinekowitsch@chello.at>
2024-05-02 23:56:37 +0200

    Update App.js

--- trdl-app/src/App.js
+++ trdl-app/src/App.js
@@ -2,11 +2,50 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>DeepApp</p>
-        <li>button</li>
-        <li>input field</li>:console.warn();
+        <center> 
+          <MyButton />
+          <MyApp />
+          <Profile />
+        </center>
       </header>          
     </div>
+  );
+}
+function MyButton() {
+  return (
+    <button>
+      Start
+    </button>
+  );
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>my app</h1>
+    </div>
+  );
+}
+
+const user = {
+  name: 'tinowitsch',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
 }
 
